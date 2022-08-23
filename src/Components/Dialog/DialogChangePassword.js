f2a986b734f87417f0f31f1de84b10ac26274c06
thangbui258 <thangbuimanh1996@gmail.com>
2022-08-23 11:49:57 +0700

    dialog change pass

---
+++ src/Components/Dialog/DialogChangePassword.js
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import {useDispatch, useSelector} from "react-redux";
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import {closeDialog} from "../../Features/DiaLogSlice/openDialogAccountSlide";
+import {closeDialogChangePass} from "../../Features/DiaLogSlice/openDialogChangePassSlice";
+import Box from "@mui/material/Box";
+import {useState} from "react";
+import axios from "axios";
+
+export default function DialogChangePassword() {
+    const [data, setData] = useState({
+        oldPassword: '',
+        newPassword: '',
+        confirmPassword: ''
+    })
+    const dispatch = useDispatch()
+    const openDialogChangePass = useSelector((state) => state.DialogPass.value)
+
+    const handleCloseChangePass = () => {
+        dispatch(closeDialogChangePass(false))
+    };
+
+    const handleChange = (e, field) => {
+        data[field] = e.target.value
+        setData({...data})
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        dispatch(closeDialogChangePass(false))
+        const body = {
+            oldPassword: data.oldPassword,
+            newPassword: data.newPassword,
+            confirmPassword: data.confirmPassword
+        }
+        console.log(body)
+        let token = JSON.parse(localStorage.getItem('token'))
+        console.log(token)
+        await axios.post('http://localhost:8080/auth/change-password',
+            body, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+
+    }
+    return (
+        <div>
+            <Dialog open={openDialogChangePass}>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                >
+                    <DialogTitle>Change Password</DialogTitle>
+                    <DialogContent>
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="oldPassword"
+                            label="Old Password"
+                            type="password"
+                            fullWidth
+                            variant="standard"
+                            onChange={(e) => handleChange(e, 'oldPassword')}
+                        />
+
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="newPassword"
+                            label="New Password"
+                            type="password"
+                            fullWidth
+                            variant="standard"
+                            onChange={(e) => handleChange(e, 'newPassword')}
+                        />
+
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            id="confirmPassword"
+                            label="Confirm Password"
+                            type="password"
+                            fullWidth
+                            variant="standard"
+                            onChange={(e) => handleChange(e, 'confirmPassword')}
+                        />
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={handleCloseChangePass}>Cancel</Button>
+                        <Button type="submit">Change</Button>
+                    </DialogActions>
+
+                </Box>
+
+            </Dialog>
+        </div>
+    );
+}
--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import clickSlide from './Features/SidebarOpenSlice/clickSlice'
+import openDialogAccountSlide from "./Features/DiaLogSlice/openDialogAccountSlide";
+import openDialogChangePassSlide from "./Features/DiaLogSlice/openDialogChangePassSlice";
 
 const store = configureStore({
     reducer:{
-        Layout: clickSlide.reducer
+        Layout: clickSlide.reducer,
+        Dialog: openDialogAccountSlide.reducer,
+        DialogPass: openDialogChangePassSlide.reducer
     }
 })
 
