fd85c490e40dd35e01c5507b5b4a2a13e236b673
pydev97@gmail.com <leqthinh20530@gmail.com>
2022-08-23 11:22:30 +0700

    create wallet-page

--- src/Pages/MyWallet/MyWallet.js
+++ src/Pages/MyWallet/MyWallet.js
@@ -1,28 +1,38 @@
 import NavbarExtra from "../../Components/NavbarExtra";
 import {useEffect, useState} from "react";
-
+import axios from 'axios'
 
 export default function MyWallet() {
-    const [wallet, setWallet] = useState()
+    const [wallets, setWallets] = useState([])
 
     useEffect(() => {
-        const axios = async () => {
-            const data =  axios.get('http://localhost:8080/wallet/render')
-        }
-        axios().then(result => {
-            console.log(result)
+        axios.get('http://localhost:8080/wallet/render').then(r => {
+            setWallets(r.data.data)
         })
-    }, [wallet])
+
+    }, [])
+    console.log(wallets)
     return (
         <div>
             <NavbarExtra/>
-            <div className="flex h-screen justify-center items-center">
-                <ul className="w-48 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-                    <li className="py-2 px-4 w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600">Profile</li>
-                    <li className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">Settings</li>
-                    <li className="py-2 px-4 w-full border-b border-gray-200 dark:border-gray-600">Messages</li>
-                    <li className="py-2 px-4 w-full rounded-b-lg">Download</li>
-                </ul>
+            <div className="flex justify-center bg-[#E4E4E4] h-[100vh]">
+                <div className=" border mt-[100px] border-none w-[664px] text-xs text-gray-900">
+                    <div
+                        className="text-left text-[#707070] px-6 py-2 border-b border-gray-200 w-[664px] h-[40px] bg-[#F4F4F4]  cursor-pointer">
+                        Included in Total
+                    </div>
+                    {wallets.map(wallet => {
+                        return(
+                            <div className="text-left hover:bg-[#E6EFE7] flex px-6 py-2 round-[10px]  w-[664px] h-[72px] bg-[#FFFFFF] text-black cursor-pointer">
+                                <img src={wallet.icon.url} className="w-10 h-10 rounded-full my-2" alt="thinh"/>
+                                <div className="px-3">
+                                <h3 className="font-sans my-1 text-[14px]">{wallet.name}</h3>
+                                <span className="text-[#949494]">+{wallet.initial} {wallet.currency.code}</span>
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
             </div>
         </div>
     )
