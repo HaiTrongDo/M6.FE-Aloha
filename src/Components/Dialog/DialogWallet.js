3b6b05c39e799637537b70bf73522e0a27b27bf9
pydev97@gmail.com <leqthinh20530@gmail.com>
2022-08-23 22:17:08 +0700

    done render wallet ui/ux

--- src/Components/Dialog/DialogWallet.js
+++ src/Components/Dialog/DialogWallet.js
@@ -1,24 +1,21 @@
-
 import {useDispatch, useSelector} from "react-redux";
 import {closeDialogWallet, openDialogWallet} from "../../Features/DiaLogSlice/openDialogMyWalletSlice";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import '../../Pages/MyWallet/MyWallet.css'
 
 export default function DialogWallet({className}) {
     const [wallets, setWallets] = useState([])
     const dispatch = useDispatch();
     const handleCloseDialogWallet = () => {
-        dispatch( closeDialogWallet(false))
+        dispatch(closeDialogWallet(false))
     }
-    console.log(wallets)
 
     useEffect(() => {
         axios.get('http://localhost:8080/wallet/render').then(r => {
             setWallets(r.data.data)
         })
-
     }, [])
-
 
     return (
         <div>
@@ -27,9 +24,11 @@ export default function DialogWallet({className}) {
             >
                 <div className="relative w-auto my-6 mx-auto max-w-3xl">
                     {/*content*/}
-                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    <div
+                        className="border-0 rounded-[5px] shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                         {/*header*/}
-                        <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                        <div
+                            className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                             <h3 className="text-3xl font-semibold">
                                 Create Wallet
                             </h3>
@@ -37,25 +36,60 @@ export default function DialogWallet({className}) {
                                 className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
                                 onClick={handleCloseDialogWallet}
                             >
-                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                    <span
+                        className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
                       ×
                     </span>
                             </button>
                         </div>
                         {/*body*/}
                         <div className="relative p-6 flex-auto">
-                            <div className="col-12">
-                                <button className="flex col-4 justify-center border border-gray-300 p-2 rounded-[20px]">
-                                    <img className="w-10 h-10 rounded-full my-2" src={wallets[0].icon.url} alt="..."/>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 my-4 mx-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+                            <div className="grid grid-cols-3 gap-3">
+                                <button
+                                    className=" flex  justify-center border border-gray-300 p-2 h-[60px] rounded-[10px] hover:border-black">
+                                    <img className="w-10 h-10 rounded-full my-0.5" src={wallets[0]?.icon?.url}
+                                         alt="..."/>
+                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                         className="h-6 w-6 my-2 mx-2 text-[#757575] hover:text-black" fill="none"
+                                         viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7"/>
                                     </svg>
-                                    </button>
-                                <input type="text" placeholder="dfdf" className="col-8"/>
+                                </button>
+                                <div className="relative col-span-2">
+                                    <input type="text" id="floating_filled"
+                                           className="block rounded-[10px] p-2 pt-5 w-[296px] h-[60px] text-sm text-gray-900 bg-gray-50  border border-gray-300  appearance-none dark:text-black  focus:outline-none focus:ring-0 hover:border-black peer"
+                                           placeholder=" "/>
+                                    <label htmlFor="floating_filled"
+                                           className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5   peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Wallet
+                                        name
+                                    </label>
+                                </div>
+                                <button
+                                    id="button"
+                                    className="col-span-2 flex relative  border border-gray-300 p-2 h-[60px]  rounded-[10px] hover:border-black">
+                                    <img className="w-[24px] h-[24px] rounded-full my-3" src={wallets[0]?.currency?.url}
+                                         alt="..."/>
+                                    <span className="my-3 mx-4">{wallets[0]?.currency.name}</span>
+                                    <label htmlFor="button" className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5   peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Currency</label>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mx-[48px] my-3 text-[#757575] hover:text-black " fill="none"
+                                         viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7"/>
+                                    </svg>
+                                </button>
+
+                                <div className="relative">
+                                    <input type="number" id="floating_filled_init"
+                                           className="block   rounded-[10px] p-2 pt-5 w-[140px] h-[60px] text-sm text-gray-900 bg-gray-50  border border-gray-300  appearance-none dark:text-black  focus:outline-none focus:ring-0 hover:border-black peer appearance-none"
+                                           placeholder=" "/>
+                                    <label htmlFor="floating_filled_init"
+                                           className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5   peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Initial Balance
+                                    </label>
+                                </div>
                             </div>
                         </div>
                         {/*footer*/}
-                        <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                        <div
+                            className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
                             <button
                                 className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                 type="button"
@@ -74,7 +108,7 @@ export default function DialogWallet({className}) {
                     </div>
                 </div>
             </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"/>
+            <div className="opacity-50 fixed inset-0 z-40 bg-black"/>
         </div>
     )
 }
