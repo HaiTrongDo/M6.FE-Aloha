6570cc91d86c716fd70239727d724556b87d5cd4
thangbui258 <thangbuimanh1996@gmail.com>
2022-08-24 00:14:03 +0700

    done fix-change-pass

--- src/Components/Dialog/DialogChangePassword.js
+++ src/Components/Dialog/DialogChangePassword.js
@@ -11,7 +11,8 @@ import {closeDialog} from "../../Features/DiaLogSlice/openDialogAccountSlide";
 import {closeDialogChangePass} from "../../Features/DiaLogSlice/openDialogChangePassSlice";
 import Box from "@mui/material/Box";
 import {useState} from "react";
-import axios from "../../axios/index";
+import axios from "axios";
+;
 
 export default function DialogChangePassword() {
     const [data, setData] = useState({
@@ -42,7 +43,7 @@ export default function DialogChangePassword() {
         console.log(body)
         let token = JSON.parse(localStorage.getItem('JWT'))
         console.log(token)
-        await axios.post('auth/change-password',
+        await axios.put('http://localhost:8080/auth/change-password',
             body, {
                 headers: {
                     Authorization: `Bearer ${token}`
