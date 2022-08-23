fcfc748ba2446ce462e46945e940ae20a63026e2
pydev97@gmail.com <leqthinh20530@gmail.com>
2022-08-23 16:19:44 +0700

    create dialog

--- src/Pages/MyWallet/MyWallet.js
+++ src/Pages/MyWallet/MyWallet.js
@@ -1,10 +1,16 @@
 import NavbarExtra from "../../Components/NavbarExtra";
+import DialogWallet from "../../Components/Dialog/DialogWallet";
 import {useEffect, useState} from "react";
 import axios from 'axios'
+import {useSelector} from "react-redux";
 
 export default function MyWallet() {
+    // const walletState = useSelector()
     const [wallets, setWallets] = useState([])
-    const [isOpen, setIsOpen] = useState(false)
+    const [className, setClassName] = useState('')
+    const walletState = useSelector((state) =>
+        state.DialogWallet.value
+    )
 
     useEffect(() => {
         axios.get('http://localhost:8080/wallet/render').then(r => {
@@ -12,21 +18,25 @@ export default function MyWallet() {
         })
 
     }, [])
+    useEffect(() => {
+        walletState ? setClassName('flex justify-center opacity-75') : setClassName('flex justify-center bg-[#E4E4E4] h-[100vh]')
+    },[walletState])
 
     return (
-        <div>
-            <NavbarExtra idModal={"defaultModal"}/>
-            <div className="flex justify-center bg-[#E4E4E4] h-[100vh]">
+
+        <div   >
+            <NavbarExtra />
+            <div  className={className}>
                 <div className=" border mt-[100px] border-none w-[664px] text-xs text-gray-900">
                     <div
-                        className="text-left text-[#707070] px-6 py-2 border-b border-gray-200 w-[664px] h-[40px] bg-[#F4F4F4]  cursor-pointer">
+                        className={className + "text-left text-[#707070] px-6 py-2 border-b border-gray-200 w-[664px] h-[40px] bg-[#F4F4F4]  cursor-pointer"}>
                         Included in Total
                     </div>
-                    {wallets.map((wallet,index) => {
+                    {wallets.map((wallet, index) => {
                         return (
                             <div key={index}
-                                className="text-left hover:bg-[#E6EFE7] flex px-6 py-2 round-[10px]  w-[664px] h-[72px] bg-[#FFFFFF] text-black cursor-pointer">
-                                <img src={wallet.icon.url} className="w-10 h-10 rounded-full my-2" alt="thinh"/>
+                                 className="text-left hover:bg-[#E6EFE7] flex px-6 py-2 round-[10px]  w-[664px] h-[72px] bg-[#FFFFFF] text-black cursor-pointer">
+                                <img src={wallet.icon.url} className="w-10 h-10 rounded-full my-2" alt=""/>
                                 <div className="px-3">
                                     <h3 className="font-sans my-1 text-[14px]">{wallet.name}</h3>
                                     <span className="text-[#949494]">+{wallet.initial} {wallet.currency.code}</span>
@@ -36,6 +46,7 @@ export default function MyWallet() {
                     })}
                 </div>
             </div>
+            {walletState && <DialogWallet/>}
         </div>
     )
 }
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import {configureStore} from '@reduxjs/toolkit'
 import clickSlide from './Features/SidebarOpenSlice/clickSlice'
 import openDialogAccountSlide from "./Features/DiaLogSlice/openDialogAccountSlide";
 import openDialogChangePassSlide from "./Features/DiaLogSlice/openDialogChangePassSlice";
+import openDialogMyWalletSlice from "./Features/DiaLogSlice/openDialogMyWalletSlice";
 import UserSlice from "./Features/CurrentUser/UserSlice";
 
 
@@ -10,7 +11,8 @@ const store = configureStore({
         Layout: clickSlide.reducer,
         Dialog: openDialogAccountSlide.reducer,
         DialogPass: openDialogChangePassSlide.reducer,
-        currentUser: UserSlice
+        currentUser: UserSlice,
+        DialogWallet:openDialogMyWalletSlice.reducer
     }
 })
 
