12d9a9310904b49364f6eb47a3ac3238625d3e1c
NQT <trungngxx@gmail.com>
2022-08-27 11:16:28 +0700

    transaction page

--- src/Pages/UserTransactionsPage/UserTransactionsPage.js
+++ src/Pages/UserTransactionsPage/UserTransactionsPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import TransactionsLayout from "../../Components/Layouts/Transactions/TransactionsLayout";
 import {useDispatch, useSelector} from "react-redux";
 import DialogTransaction from "../../Components/Dialog/DialogTransaction";
@@ -7,6 +7,7 @@ import DialogSelectWallet from "../../Components/Dialog/DialogSelectWallet";
 import {closeDialogTransaction} from "../../Features/DiaLogSlice/openDialogTransactionSlice";
 import {closeDialogSelectWallet} from "../../Features/DiaLogSlice/openDialogWallet";
 import Button from '@mui/material/Button';
+import axios from "../../axios";
 
 
 const UserTransactionsPage = () => {
@@ -14,10 +15,27 @@ const UserTransactionsPage = () => {
     const transactionState = useSelector(state => state.dialogTransaction.value);
     const dialogCategoryState = useSelector(state => state.DialogCategory.value)
     const dialogWalletState = useSelector(state => state.dialogWallet.value);
-    const [openDetail, setOpenDetail] = useState(true)
+    const [toggleDetail, setToggleDetail] = useState(false)
+    const [active] = useState("py-3 sm:py-4 hover:bg-emerald-50 hover:cursor-pointer")
+    const [listTransaction, setListTransaction] = useState([])
+    const user = JSON.parse(localStorage.getItem('alohaUser'))
+    const [detail, setDetail] = useState({})
+
+
+    useEffect(() => {
+        // goi API cot ben trai
+        axios.post('transaction/list', {user: user._id})
+            .then(res => {
+                setListTransaction(res.data.data)
+            })
+    }, [])
 
     const handleCLoseDetail = () => {
-        setOpenDetail(false)
+        setToggleDetail(false)
+    }
+    const handleOpenDetail = () => {
+        console.log('detail', detail)
+        setToggleDetail(true)
     }
 
     return (
@@ -59,48 +77,62 @@ const UserTransactionsPage = () => {
                             className=" w-full bg-white border sm:p-8 ">
                             <div className="flow-root w-full ">
                                 <ul role="list" className="divide-y divide-gray-200 ">
-                                    <li className="py-3 sm:py-4">
-                                        <div className="flex items-center space-x-4">
-                                            <div className="flex-shrink-0">
-                                                <img className="w-8 h-8 rounded-full"
-                                                     src="https://static.moneylover.me/img/icon/icon_136.png"
-                                                     alt="Neil image"/>
-                                            </div>
-                                            <div className="flex-1 min-w-0">
-                                                <p className="text-sm font-medium text-gray-900 truncate ">
-                                                    Transportation
-                                                </p>
-                                                <p className="text-sm text-gray-500 truncate ">
-                                                    2 Transactions
-                                                </p>
-                                            </div>
-                                            <div
-                                                className="inline-flex items-center text-base  text-gray-900 ">
-                                                -$ 212.00
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li className="py-3 sm:py-4">
-                                        <div className="flex items-center space-x-4">
-                                            <div className="flex-shrink-0">
-                                                <div className="text-2xl"
-                                                >25
+
+                                    {listTransaction.map((transaction, index) => (
+                                        <div key={index}>
+                                            <li className="py-3 sm:py-4">
+                                                <div className="flex items-center space-x-4">
+                                                    <div className="flex-shrink-0">
+                                                        <img className="w-8 h-8 rounded-full"
+                                                             src="https://static.moneylover.me/img/icon/icon_136.png"
+                                                             alt="Neil image"/>
+                                                    </div>
+                                                    <div className="flex-1 min-w-0">
+                                                        <p className="text-sm font-medium text-gray-900 truncate ">
+                                                            {transaction.category.name}
+                                                        </p>
+                                                        <p className="text-sm text-gray-500 truncate ">
+                                                            2 Transactions
+                                                        </p>
+                                                    </div>
+                                                    <div
+                                                        className="inline-flex items-center text-base  text-gray-900 ">
+                                                        {transaction.category.type === 'EXPENSE'
+                                                            ? "-" + transaction.amount
+                                                            : "+" + transaction.amount}
+                                                    </div>
                                                 </div>
-                                            </div>
-                                            <div className="flex-1 min-w-0">
-                                                <p className="text-sm font-medium text-gray-900 truncate ">
-                                                    Transportation
-                                                </p>
-                                                <p className="text-sm text-gray-500 truncate ">
-                                                    2 Transactions
-                                                </p>
-                                            </div>
-                                            <div
-                                                className="inline-flex items-center text-base  text-gray-900 ">
-                                                -$ 212.00
-                                            </div>
+                                            </li>
+                                            <li className={!toggleDetail ? active : active + " " + "bg-emerald-50"}
+                                                onClick={() => {
+                                                    setDetail(transaction);
+                                                    handleOpenDetail()
+                                                }}>
+                                                <div className="flex items-center space-x-4">
+                                                    <div className="flex-shrink-0">
+                                                        <div className="text-2xl"
+                                                        >25
+                                                        </div>
+                                                    </div>
+                                                    <div className="flex-1 min-w-0">
+                                                        <p className="text-sm text-gray-500 truncate ">
+                                                            {transaction.date}
+                                                        </p>
+                                                    </div>
+                                                    <div
+                                                        className={transaction.category.type === 'EXPENSE'
+                                                            ? 'inline-flex items-center text-base text-red-600'
+                                                            : 'inline-flex items-center text-base text-blue-600 '}
+                                                    >
+                                                        {transaction.category.type === 'EXPENSE'
+                                                            ? "-" + transaction.amount
+                                                            : "+" + transaction.amount}
+                                                    </div>
+                                                </div>
+                                            </li>
                                         </div>
-                                    </li>
+                                    ))
+                                    }
                                 </ul>
                             </div>
                         </div>
@@ -108,31 +140,58 @@ const UserTransactionsPage = () => {
 
 
                     {/*detail transaction*/}
-                    {openDetail &&
-                        <div className=" master-container flex h-[296px] w-[60%] rounded-5 ">
-                            <div className="bg-white w-full h-[296px]">
-                                <div className="flex items-start p-5 border-0 rounded-t border-b-2">
-                                    <button className="pt-1 text-[#757575]"
-                                            onClick={handleCLoseDetail}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
-                                             viewBox="0 0 24 24"
-                                             stroke="currentColor" strokeWidth="2">
-                                            <path strokeLinecap="round" strokeLinejoin="round"
-                                                  d="M6 18L18 6M6 6l12 12"></path>
-                                        </svg>
-                                    </button>
-                                    <span className="pl-[15px] text-[20px] font-sans">
-                                        Transaction Details
-                                    </span>
-                                    <div>
-                                        <Button sx={{color:'red'}}>DELETE</Button>
-                                        <Button>EDIT</Button>
+                    {toggleDetail &&
+                        <div className=" master-container flex h-[280px] w-[60%] rounded-5">
+                            <div className="bg-white w-full ">
+                                <div className="flex justify-between items-start p-5 border-0 rounded-t border-b-2">
+                                    <div className="inline flex ml-4">
+                                        <button className="pt-1 text-[#757575] my-auto"
+                                                onClick={handleCLoseDetail}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
+                                                 viewBox="0 0 24 24"
+                                                 stroke="currentColor" strokeWidth="2">
+                                                <path strokeLinecap="round" strokeLinejoin="round"
+                                                      d="M6 18L18 6M6 6l12 12"></path>
+                                            </svg>
+                                        </button>
+                                        <div className="pl-[15px] text-[20px] h-6 font-sans ml-2 font-semibold">
+                                            Transaction Details
+                                        </div>
+                                    </div>
+                                    <div className="">
+                                        <Button sx={{color: 'red'}}>DELETE</Button>
+                                        <Button sx={{color: '#2EB74B'}}>EDIT</Button>
                                     </div>
                                 </div>
 
 
                                 <div>
+                                    <div className="grid grid-cols-6 mt-3">
+                                        <div className="">
+                                            <img src="https://static.moneylover.me/img/icon/ic_category_foodndrink.png"
+                                                 alt=""
+                                                 className="w-[60px] ml-14"/>
+                                        </div>
+                                        <div className="col-span-5">
+                                            <div className="text-3xl">{detail.category.name}</div>
+                                            <div className="mt-1 ">Ăn uống</div>
+                                            <div className="mt-1 text-gray-500">{detail.date}</div>
+                                            <hr className="mt-2 w-[200px]"/>
+                                        </div>
+                                    </div>
 
+                                    <div className="grid grid-cols-6">
+                                        <div></div>
+                                        <div
+                                            className={detail.category.type === 'EXPENSE'
+                                                ? 'text-3xl text-red-600 mt-4 col-span-5'
+                                                : 'text-3xl text-blue-600 mt-4 col-span-5'}
+                                        >
+                                            {detail.category.type === 'EXPENSE'
+                                                ? '-' + detail.amount
+                                                : '+' + detail.amount}
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
