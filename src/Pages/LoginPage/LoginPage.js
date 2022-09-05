32cfd6886cd5dd6c885db9929aa6c57f838b87d0
pydev97@gmail.com <leqthinh20530@gmail.com>
2022-09-05 12:23:47 +0700

    update ui

--- src/Pages/LoginPage/LoginPage.js
+++ src/Pages/LoginPage/LoginPage.js
@@ -167,10 +167,12 @@ const LoginPage = () => {
                         <h1>Create Account</h1>
                         <div className="social-container">
                             <Link to="" onClick={() => signInWithFireBase(auth, faceBookAuthProvider)}
-                                  className="social"><i className="fab fa-facebook-f"/></Link>
-                            <Link to="" onClick={() => signInWithFireBase(auth, googleAuthProvider)} className="social"><i
+                                  className="social hover:text-blue-700"><i className="fab fa-facebook-f"/></Link>
+                            <Link to="" onClick={() => signInWithFireBase(auth, googleAuthProvider)}
+                                  className="social hover:text-red-700"><i
                                 className="fab fa-google-plus-g"/></Link>
-                            <Link to="" onClick={() => signInWithFireBase(auth, githubAuthProvider)} className="social"><i
+                            <Link to="" onClick={() => signInWithFireBase(auth, githubAuthProvider)}
+                                  className="social hover:text-black-700"><i
                                 className="fab fa-linkedin-in"/></Link>
                         </div>
                         <span style={{margin: '10px'}}>or use your email for registration</span>
@@ -180,7 +182,26 @@ const LoginPage = () => {
                         <input type="email" name='email' placeholder="Email" onChange={handleChangeSignUp}/>
                         {validateSignUpMsg.email &&
                             <p className='text-red-500 text-xs italic'>{validateSignUpMsg.email}</p>}
-                        <input type="password" name='password' placeholder="Password" onChange={handleChangeSignUp}/>
+                        <div className={"relative w-full"}>
+                            <input type={togglePassword ? "password" : "text"} name='password' placeholder="Password"
+                                   onChange={handleChangeSignUp}/>
+                            <div onClick={handleChangeTypePassword}
+                                 className={"absolute cursor-pointer inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"}>
+                                {togglePassword ?
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/>
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                                    </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                  strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"/>
+                                    </svg>
+                                }
+                            </div>
+                        </div>
                         {validateSignUpMsg.password &&
                             <p className='text-red-500 text-xs italic'>{validateSignUpMsg.password}</p>}
                         <button style={{marginTop: '10px'}} onClick={handleSignUp}>Sign Up</button>
@@ -191,10 +212,12 @@ const LoginPage = () => {
                         <h1>Sign in</h1>
                         <div className="social-container">
                             <Link to="" onClick={() => signInWithFireBase(auth, faceBookAuthProvider)}
-                                  className="social"><i className="fab fa-facebook-f"/></Link>
-                            <Link to="" onClick={() => signInWithFireBase(auth, googleAuthProvider)} className="social"><i
+                                  className="social hover:text-blue-700"><i className="fab fa-facebook-f"/></Link>
+                            <Link to="" onClick={() => signInWithFireBase(auth, googleAuthProvider)}
+                                  className="social hover:text-red-700"><i
                                 className="fab fa-google-plus-g"/></Link>
-                            <Link to="" onClick={() => signInWithFireBase(auth, githubAuthProvider)} className="social"><i
+                            <Link to="" onClick={() => signInWithFireBase(auth, githubAuthProvider)}
+                                  className="social hover:text-black"><i
                                 className="fab fa-github"/></Link>
                         </div>
                         <span style={{margin: '10px'}}>or use your account</span>
@@ -203,17 +226,24 @@ const LoginPage = () => {
                         {validateSignInMsg.email &&
                             <p className='text-red-500 text-xs italic'>{validateSignInMsg.email}</p>}
                         <div className={"w-full relative "}>
-                            <input type={togglePassword ? "password" : "text"} name='password' placeholder="Password" onChange={handleChangeSignIn}
+                            <input type={togglePassword ? "password" : "text"} name='password' placeholder="Password"
+                                   onChange={handleChangeSignIn}
                             />
                             <div onClick={handleChangeTypePassword}
                                  className={"absolute cursor-pointer inset-y-0 right-0 pr-3 flex items-center text-sm leading-5"}>
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                     strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
-                                    <path strokeLinecap="round" strokeLinejoin="round"
-                                          d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/>
-                                    <path strokeLinecap="round" strokeLinejoin="round"
-                                          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
-                                </svg>
+                                {togglePassword ?
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/>
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
+                                    </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                  strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round"
+                                              d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"/>
+                                    </svg>
+                                }
                             </div>
                             {validateSignInMsg.password &&
                                 <p className='text-red-500 text-xs italic'>{validateSignInMsg.password}</p>}
