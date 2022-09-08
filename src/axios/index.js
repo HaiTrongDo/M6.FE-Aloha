93e72babedfa2675632a23a34994a9b67c444040
HaiTrongDo <Dotronghai91@gmail.com>
2022-09-08 14:36:06 +0700

    update

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: "http://localhost:8080/"
     baseURL: "http://aloha-back-end.herokuapp.com/"
+    // baseURL: "http://localhost:8080/"
 })
